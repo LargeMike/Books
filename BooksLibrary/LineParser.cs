08ff63ef363116c2c5f21d6eb14e0763f9a4bc3f
Mike Beka <mikhail.beka@outlook.com>
2026-04-14 15:31:10 +0300

    parser changed

--- BooksLibrary/LineParser.cs
+++ BooksLibrary/LineParser.cs
@@ -6,8 +6,20 @@ public class LineParser
     {
         var values = line.Split(',');
         var book = new ParsedBook();
-        var property = typeof(ParsedBook).GetProperty(nameof(ParsedBook.Title));
         
+        for (int i = 0; i < headers.Length; i++)
+        {
+            try
+            {
+                var property = typeof(ParsedBook).GetProperty(headers[i]);
+                property.SetValue(book, Convert.ChangeType(values[i], Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType));
+            }
+            catch (FormatException)
+            {
+                book.NotParsedDate = values[i];
+            }
+        }
+
         return book;
     }
 }
--- BooksLibrary/ParsedBook.cs
+++ BooksLibrary/ParsedBook.cs
@@ -3,9 +3,10 @@ namespace BooksLibrary;
 public class ParsedBook
 {
     public string Title { get; set; }
-    public string AuthorName { get; set; }
-    public string GenreName { get; set; }
-    public string PublisherName { get; set; }
-    public int NumberOfPages { get; set; }
-    public DateTime DatePublished { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public string Publisher { get; set; }
+    public int Pages { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public string? NotParsedDate {get; set; }
 }
